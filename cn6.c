f99bac9af452aa0c63a7c0208269663102690abd
Dayananda Sagar College Of Enginering <159140966+dscelab@users.noreply.github.com>
2024-12-13 23:03:46 +0530

    pm6

--- cn6.c
+++ cn6.c
@@ -1,63 +1,51 @@
 #include<stdio.h>
-#include<stdlib.h>
-void rout_table();
-int d[10][10],via[10][10];
-int i,j,k,l,m,n,g[10][10],temp[10][10],ch,cost;
-
+#include<string.h>
+#define N strlen(g)
+char t[128],cs[128],g[]="10001000000100001";
+int a,e,c;
+void xor()
+{
+for(c=1;c<N;c++)
+cs[c]=((cs[c]==g[c])?'0':'1');
+}
+void crc()
+{
+for(e=0;e<N;e++)
+cs[e]=t[e];
+do
+{
+if(cs[0]=='1')
+xor();
+for(c=0;c<N-1;c++)
+cs[c]=cs[c+1];
+cs[c]=t[e++];
+}while(e<=a+N-1);
+}
 int main()
 {
-  system("clear");
-  printf("enter the value of no of nodes\n");
-  scanf("%d",&n);
-  rout_table();
-  for(i=0;i<n;i++)
-  for(j=0;j<n;j++)
-     temp[i][j]=g[i][j];
-  for(i=0;i<n;i++)
-  for(j=0;j<n;j++)
-     via[i][j]=i;
-  while(1)
-  {
-     for(i=0;i<n;i++)
-     for(j=0;j<n;j++)
-     if(d[i][j])
-     for(k=0;k<n;k++)
-     if(g[i][j]+g[j][k]<g[i][k])
-     {
-       g[i][k]=g[i][j]+g[j][k];
-       via[i][k]=j;
-     }
-     for(i=0;i<n;i++)
-     {
-       printf("table for router %c\n",i+97);
-       for(j=0;j<n;j++)
-          printf("%c::%d via %c\n",j+97,g[i][j],via[i][j]+97);
-         }
-       break;
-      }
-      }
-      
-      void rout_table()
-      {
-        printf("\nEnter the routing table:\n");
-        printf("\t");
-        for(i=1;i<=n;i++)
-           printf("%c\t",i+96);
-        printf("\n");
-
-        for(i=0;i<=n;i++)
-          printf("-------------");
-        printf("\n");
-
-        for(i=0;i<n;i++)
-        {
-          printf("%c  ",i+97);
-
-          for(j=0;j<n;j++)
-          {
-           scanf("%d",&g[i][j]);
-            if(g[i][j]!=999)
-            d[i][j]=1;
-           }
-         }
-      }
+printf("\nEnter poly : ");
+scanf("%s",t);
+printf("\nGenerating Polynomial is : %s",g);
+a=strlen(t);
+for(e=a;e<a+N-1;e++) t[e]='0';
+printf("\nModified t[u] is : %s",t);
+crc();printf("\nChecksum is : %s",cs);
+for (e=a;e<a+N-1;e++) t[e]=cs[e-a];
+printf("\nFinalCodeword is : %s",t);
+printf("\nTest Error detection 0(yes) 1(no) ? : ");
+scanf("%d",&e);
+if (e==0)
+{
+printf("Enter position where error is to inserted : ");
+scanf("%d",&e);
+t[e]=(t[e]=='0')?'1':'0';
+printf("Errorneous data: %s\n",t);
+}
+crc();
+for(e=0;(e<N-1)&&(cs[e]!='1');e++);
+if(e<N-1)
+printf("Error detected.");
+else
+printf("No Error Detected.");
+return 0;
+}
